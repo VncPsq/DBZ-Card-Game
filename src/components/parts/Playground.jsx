97f2cf8001003981850cc6af03484ba54a68d237
Vincent Pasquiou <vincent.pasquiou@gmail.com>
2024-05-07 23:49:08 +0200

    flipped is isolated

--- src/components/parts/Playground.jsx
+++ src/components/parts/Playground.jsx
@@ -1,9 +1,40 @@
+import { useState, useEffect } from "react";
 import Card from "../features/Card";
 
 function Playground() {
+  const [characters, setCharacters] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://dragonball-api.com/api/characters?limit=50")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        return response.json();
+      })
+      .then((items) => {
+        const characters = items.items;
+        setCharacters(characters);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching data", error);
+      });
+  }, []);
+
   return (
     <>
-      <Card />
+      {!isLoading ? (
+        characters.map((character) => (
+          <Card
+            key={`Card ${character.id} | ${character.name}`}
+            character={character}
+          />
+        ))
+      ) : (
+        <p>Loading...</p>
+      )}
     </>
   );
 }
