3eb23eb440ff4e17c5e3e5830275e57d4d49f806
Vincent Pasquiou <vincent.pasquiou@gmail.com>
2024-05-10 17:08:37 +0200

    create ModalePlayground

--- src/components/parts/Playground.jsx
+++ src/components/parts/Playground.jsx
@@ -1,11 +1,13 @@
 import { useApi } from "../../contexts/ApiContext";
 import Card from "../features/Card";
+import radar from "../../assets/img/radar.png";
+import Button from "../../components/utilities/Button";
 
 function Playground() {
   const { characters, isLoading } = useApi();
 
   return (
-    <>
+    <article className="playground">
       {!isLoading ? (
         characters.map((character) => (
           <Card
@@ -16,7 +18,14 @@ function Playground() {
       ) : (
         <p>Loading...</p>
       )}
-    </>
+      <section className="modalePlayground">
+        <article className="modaleContent">
+          <h3>Hurry up, she's in danger !</h3>
+          <img src={radar} alt="Dragon Ball radar" />
+          <button>Save Bulma</button>
+        </article>
+      </section>
+    </article>
   );
 }
 
