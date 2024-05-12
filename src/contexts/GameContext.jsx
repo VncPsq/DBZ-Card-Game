ff0373c64b055d4a6010b6acc112ffe11186eb64
Vincent Pasquiou <vincent.pasquiou@gmail.com>
2024-05-12 13:20:43 +0200

    fix points earn

--- src/contexts/GameContext.jsx
+++ src/contexts/GameContext.jsx
@@ -5,13 +5,12 @@ const GameContext = createContext();
 export function GameProvider({ children }) {
   const [startLevel, setStartLevel] = useState(false); // Init level(unshowModal, spinDragonBall, showScore )
   const [score, setScore] = useState(0); // Init Score
-  const [level, setLevel] = useState(0); // Init Level
+  const [level, setLevel] = useState(1); // Init Level
   const [dragonBalls, setDragonBalls] = useState(7); // Init DragonBalls
   const [resetCards, setResetCards] = useState(false); // Reset flipped cards
 
   const handleNewLevel = () => {
     setStartLevel(true);
-    setLevel((prevState) => prevState + 1);
     setResetCards(false);
   }; // Init movement DragonBall and UnshowModal and Level Number and force return BackCard
 
@@ -24,16 +23,21 @@ export function GameProvider({ children }) {
     ) {
       setScore((prevState) => prevState - 300);
     } else if (name === "Freezer") {
-      setScore((prevState) => prevState - 1000);
+      setScore((prevState) => prevState - 300);
       setDragonBalls((prevState) => prevState - 1);
       setStartLevel(false);
       setTimeout(() => {
         setResetCards(true);
-      }, 3000);
+      }, 2000);
     } else if (name === "Bulma") {
-      setScore((prevState) => prevState + 1000);
+      setScore((prevState) => prevState + 500);
+      setLevel((prevState) => prevState + 1);
+      setStartLevel(false);
+      setTimeout(() => {
+        setResetCards(true);
+      }, 2000);
     } else {
-      setScore((prevState) => prevState + 300);
+      setScore((prevState) => prevState + 500);
     }
   }; // Flip the card and handle infos character to set dynamic score
 
